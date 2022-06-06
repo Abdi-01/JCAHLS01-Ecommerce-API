2d452b38d25d5d374a0004f4242fd99171cebfe7
Abdi-01 <abdialghi@gmail.com>
2022-06-06 22:06:19 +0700

    multer

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(express.static('public'));
 app.use(cors());
 // DB Check Connection
 const { dbConf } = require('./config/database')
